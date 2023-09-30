9cd8d1786522e3d5228a9c3f260dc274c9f7d54a
Lisa-2011 <93234101+Lisa-2011@users.noreply.github.com>
2023-09-30 10:22:54 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+difference=0;
+leftWristX=0;
+rightWristX=0;
+
+
 function setup(){
     video=createCapture(VIDEO);
     video.size(550,500);    
@@ -12,9 +17,20 @@ function setup(){
 function gotPoses(results){
     if(results.length>0){
         console.log(results);
+        rightWristX=results[0].pose.rightWrist.x;
+        leftWristX=results[0].pose.leftWrist.y;
+        difference=Math.floor(leftWristX-rightWristX);
+
     }
 }
 
 function modelLoaded(){
     console.log("poseNet is initialized");
+}
+
+function draw(){
+    background('#87CEEB');
+    fill('#FFFFFF');
+    textSize(difference);
+    text('Lisa',20,220);
 }
